fd19c35bde82eb45f24518f4229a7fe480671d3f
NoPrejudiceNTolerance <antoine.malinet@orange.fr>
2018-05-16 21:29:34 -0400

    Merge handling

--- model/Musician.java
+++ model/Musician.java
@@ -4,16 +4,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Observable;
 
-<<<<<<< HEAD
-public class Musician implements Serializable{
+public class Musician extends Competitor implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-=======
-public class Musician extends Competitor {
->>>>>>> 5ecc8cee1692ce7a9ee6883af37e55861973e809
 	static private ArrayList<Musician> instances;
 	
 	private ArrayList<Instrument> instruments;
@@ -31,21 +27,10 @@ public class Musician extends Competitor {
 	public static ArrayList<Musician> getInstances() {
 		return instances;
 	}
-	
-<<<<<<< HEAD
 	public static void setInstances(ArrayList<Musician> musicians) {
 		instances = musicians;
 	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-=======
->>>>>>> 5ecc8cee1692ce7a9ee6883af37e55861973e809
+
 	public ArrayList<Instrument> getInstruments() {
 		return instruments;
 	}
